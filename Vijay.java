7c775ac9916fd284963a9449ea0489b964d09b3d
vijay-seetharam3 <seetharamvijay3@gmail.com>
2023-09-28 21:58:38 +0530

    biggest of all code in java is added

---
+++ Vijay.java
@@ -0,0 +1,14 @@
+public class Vijay {
+  public static void main(String[] args) {
+	int a = 1000;
+	int b = 349;
+	int c = 7;
+	if(a>b && a>c) {
+    		System.out.println("a is bigger");
+	}else if(b>c) {
+    		System.out.println("b is bigger");
+	}else {
+		System.out.println("c is bigger");
+}	
+  }
+}
